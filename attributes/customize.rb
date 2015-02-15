f8adba48fd7a8570c05d9fe47338b7affa53c93d
Jason Wieringa <jason@jasonwieringa.com>
2015-02-15 16:37:10 -0500

    Upgrade ES to 1.4.2

--- attributes/customize.rb
+++ attributes/customize.rb
@@ -5,5 +5,7 @@
 #
 # The following shows how to override the Elasticsearch version and cluster name settings:
 #
-# normal[:elasticsearch][:version] = '1.1.0'
+normal[:elasticsearch][:version] = '1.4.2'
 # normal[:elasticsearch][:cluster][:name] = 'my-cluster'
+
+normal['elasticsearch']['plugins']['elasticsearch/elasticsearch-cloud-aws']['version'] = '2.4.1'
